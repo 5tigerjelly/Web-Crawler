3c7c72d9802bd554992c5920ed4c9f4ceff30c88
iGuest <5tigerjelly@gmail.com>
2016-05-02 21:51:15 -0700

    add files

---
+++ WebRole1/admin.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="admin.asmx.cs" Class="WebRole1.WebService1" %>
---
+++ WebRole1/admin.asmx.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebRole1
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string startCrawl()
+        {
+            Queue<string> que = new Queue<string>();
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                ConfigurationManager.AppSettings["StorageConnectionString"]);
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+            CloudQueue queue = queueClient.GetQueueReference("myurl");
+            queue.CreateIfNotExists();
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public string stopCrawl()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public string clearIndex()
+        {
+            //table storage
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public string getPageTitle()
+        {
+            //type in URL
+            //grab title of the html from table index
+            return "Hello World";
+        }
+    }
+}
